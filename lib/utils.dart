d64de35c9a98565d7069e944f70c1b85552f7d48
Daniel Kim <dwkim0101@naver.com>
2023-02-28 16:39:46 +0900

    hive working on

--- lib/utils.dart
+++ lib/utils.dart
@@ -20,8 +20,8 @@ class Event {
   bool checkState = false;
 
   @HiveField(2)
-  late Map<DateTime, bool> reviewState;
-  // TODO: event data / event delete screen / landing page
+  Map<DateTime, bool> reviewState = {};
+  // TODO: event delete screen / landing page
 
   Event(this.title);
 
@@ -57,14 +57,11 @@ class RepeatableEvent {
   String toString() => title;
 }
 
-/// Example events.
-///
-/// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
 LinkedHashMap<DateTime, List<Event>> kEvents =
     LinkedHashMap<DateTime, List<Event>>(
   equals: isSameDay,
   hashCode: getHashCode,
-)..addAll(_kEventSource);
+);
 
 LinkedHashMap<DateTime, List<Event>> dailyEvents =
     LinkedHashMap<DateTime, List<Event>>(
@@ -74,16 +71,16 @@ LinkedHashMap<DateTime, List<Event>> dailyEvents =
 
 List<RepeatableEvent> repeatingEvents = [];
 
-final _kEventSource = {
-  for (var item in List.generate(50, (index) => index))
-    DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5): List.generate(
-        item % 4 + 1, (index) => Event('Event $item | ${index + 1}'))
-}..addAll({
-    kToday: [
-      Event('Today\'s Event 1'),
-      Event('Today\'s Event 2'),
-    ],
-  });
+// final _kEventSource = {
+//   for (var item in List.generate(50, (index) => index))
+//     DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5): List.generate(
+//         item % 4 + 1, (index) => Event('Event $item | ${index + 1}'))
+// }..addAll({
+//     kToday: [
+//       Event('Today\'s Event 1'),
+//       Event('Today\'s Event 2'),
+//     ],
+//   });
 
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
